d747f042a505babd3dd4bf8784c27257119ba12b
gitLization <gitLization@gmail.com>
2021-04-06 22:35:08 +0900

    code review

--- week04/hamburger/js/main.js
+++ week04/hamburger/js/main.js
@@ -2,15 +2,17 @@
 const menuOpenEl = document.getElementById('btn--open')
 // 닫는 아이콘 선택자
 const menuCloseEl = document.getElementById('btn--close')
-// 사이드 메뉴 선택자
-const sideMenuEl = document.getElementById('nav_menu')
+// 사이드 네이게이션 선택자
+const sideMenuEl = document.getElementById('side__menu')
+
 menuOpenEl.addEventListener("click", function() {
-    menuOpenEl.style.diaplay="none"
-    menuCloseEl.style.diaplay="block"
+    menuOpenEl.style.diaplay='none'
+    menuCloseEl.style.diaplay='block'
     sideMenuEl.style.transform='translate(0)'
 })
+
 menuCloseEl.addEventListener("click", function() {
-    menuCloseEl.style.diaplay="none"
-    menuOpenEl.style.diaplay="block"
+    menuOpenEl.style.diaplay='block'
+    menuCloseEl.style.diaplay='none'
     sideMenuEl.style.transform='translate(-100)'
 })
